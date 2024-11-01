71e030e840ec9bd1967bf1967c8913c9bd80c804
GiuliaZ538 <60102859+GiuliaZ538@users.noreply.github.com>
2024-11-01 14:05:05 +0100

    Update MetaDMG analysis.sh

--- scripts/MetaDMG analysis.sh
+++ scripts/MetaDMG analysis.sh
@@ -1,8 +1,8 @@
 #MetaDMG analysis
-###Creating config file
+##Creating config file
 metaDMG config /path/to/files/*.bam --names path/to/ncbi_taxonomy/names.dmp --nodes path/to/ncbi_taxonomy/nodes.dmp --acc2tax path/to/ncbi_taxonomy/accession2taxid.gz
 
-###Config file settings
+##Config file settings
 samples: file_name_no_duplicates: /path/to/file_no_duplicates.bam
 metaDMG_cpp: /path/to/metaDMG-cpp/metaDMG-cpp
 names: path/to/ncbi_taxonomy/names.dmp
@@ -26,5 +26,5 @@ config_file: config.yaml
 damage_mode: global
 version: 0.24.7
 
-###Generate final output
+##Generate final output
 metaDMG convert --output /path/to/final_output.csv --results /path/to/results_folder/results
